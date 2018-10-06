e386ec569bcc080db3dbdfb8f0cc34d49f32e3a4
Mike-415 <mgonz160@mail.ccsf.edu>
2018-10-06 13:37:40 -0700

    Implemented URI matcher in BookProvider class

--- app/src/main/java/com/example/android/bookinventory/data/BookProvider.java
+++ app/src/main/java/com/example/android/bookinventory/data/BookProvider.java
@@ -4,6 +4,7 @@ package com.example.android.bookinventory.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -14,6 +15,23 @@ public class BookProvider extends ContentProvider {
 
     private BooksDbHelper dbHelper;
 
+    /** URI matcher code for the content URI for the books table */
+    private static final int BOOKS = 1;
+
+    /** URI matcher code for the content URI for a single book in the books table */
+    private static final int BOOK_ID = 2;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS, BOOKS);
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS+"/#", BOOK_ID);
+    }
 
     @Override
     public boolean onCreate() {
